0bce3abd216733b327022d4ee58dfe0934d335ac
TherRipper482 <santiagogustavo882@gmail.com>
2025-03-27 06:59:40 -0600

    06 Landlord page

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+    return (
+        <div className="met-6 py-4 px-6 cursor-pointer bg-airbnb text-while rounded-xl hover:bg-airbnb-dark transition">
+            Button
+        </div>
+    )
+}
+
+export default ContactButton;
