9f9411fa87424bfa654a5e43a8acdfbf3569d60f
TherRipper482 <santiagogustavo882@gmail.com>
2025-03-27 20:29:12 -0600

    12 create reusable modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
-import UserNav from "./components/navbar/UserNav";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +16,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>yo</p>
+  )
+
   return (
     <html lang="en">
       <body className={inter.className}>
